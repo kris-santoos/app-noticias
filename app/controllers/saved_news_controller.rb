ecd92b7433357d3dd477faf7469b24c9875ca8b4
Krisllayne Santos <krisllayne00@outlook.com>
2025-08-07 09:28:06 -0300

    Adiciona relacionamento entre artigo salvo e usuário logado

--- app/controllers/saved_news_controller.rb
+++ app/controllers/saved_news_controller.rb
@@ -1,20 +1,39 @@
 class SavedNewsController < ApplicationController
+  
+  before_action :require_logged_in_user
+  
   def index
-    @saved_news = SavedNews.all
+    @saved_news = current_user.saved_news
   end
 
 def create
-  @saved_news = SavedNews.new(saved_news_params)
+  @saved_news = current_user.saved_news.build(saved_news_params)
 
   if @saved_news.save
     respond_to do |format|
-      format.html { redirect_to articles_path, notice: "Notícia salva com sucesso!" }
+      format.html do
+        flash[:success] = "Notícia salva com sucesso!"
+        redirect_to articles_path
+      end
+
       format.js   # Vai buscar o create.js.erb
     end
   else
+    puts @saved_news.errors.full_messages.inspect
     respond_to do |format|
-      format.html { redirect_to articles_path, alert: "Erro ao salvar notícia." }
-      format.js
+      if @saved_news.errors.full_messages.any? { |msg| msg.include?("Url já cadastrado") }
+        format.html do
+          flash[:danger] = "Esse artigo já foi salvo."
+          redirect_to articles_path
+        end
+      else
+        format.html do
+          flash[:danger] = "Erro ao salvar notícia."
+          redirect_to articles_path
+        end
+
+        format.js
+      end
     end
   end
 end
--- app/models/saved_news.rb
+++ app/models/saved_news.rb
@@ -1,2 +1,9 @@
 class SavedNews < ApplicationRecord
+
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :url, presence: true, uniqueness: { scope: :user_id}
+
+    belongs_to :user
+    
 end
