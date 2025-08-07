48d8b2a267891592ead124bcd68f55432adfb94d
Krisllayne Santos <krisllayne00@outlook.com>
2025-08-07 09:43:33 -0300

    Atualiza schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_05_231741) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_06_221405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_05_231741) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_saved_news_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_05_231741) do
   add_foreign_key "article_topics", "articles"
   add_foreign_key "article_topics", "topics"
   add_foreign_key "articles", "users"
+  add_foreign_key "saved_news", "users"
 end
